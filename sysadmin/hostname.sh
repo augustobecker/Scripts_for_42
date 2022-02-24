47f0accc1f70a3fba415e4bed493942fa28a6e2d
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-24 11:51:27 -0300

    Create hostname.sh

---
+++ sysadmin/hostname.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+#                                                                            
+#                                                       :::      ::::::::    
+#   hostname.sh                                        :+:      :+:    :+:      
+#                                                    +:+ +:+         +:+     
+#   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        
+#                                                +#+#+#+#+#+   +#+           
+#   Created: 2022/01/30 16:24:19 by acesar-l          #+#    #+#             
+#   Updated: 2022/02/24 11:54:22 by acesar-l         ###   ########.fr       
+#                                                                            
+#
+
+NEW_HOSTNAME=$1
+
+GREEN="\033[0;32m"
+RESET="\033[0m"
+
+cd /
+sed -i "s/$HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+echo $NEW_HOSTNAME > /etc/hostname
+echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
+echo -e "Hostname was changed to"$GREEN $NEW_HOSTNAME $RESET
